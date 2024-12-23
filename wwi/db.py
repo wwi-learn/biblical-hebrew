4bbbb7bab17ddc9d3d130eaacc42871b3e8ee2c2
Blankyc <kristov.kok@gmail.com>
2024-12-23 17:18:41 +0200

    progress

--- wwi/db.py
+++ wwi/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
+from sqlalchemy import Column, Integer, String, create_engine, ForeignKey, UniqueConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, relationship
 
@@ -6,37 +6,52 @@ Base = declarative_base()
 
 
 class Word(Base):
-    __tablename__ = 'words'
+    __tablename__ = 'word'
     id = Column(Integer, primary_key=True, autoincrement=True)
-    root = Column(String, nullable=False)
+    hebrew = Column(String, nullable=False, unique=True)
+    root = Column(String)
     language = Column(String, nullable=False)
-    word_type = Column(String, nullable=False)
-    word_subtype = Column(String)
+    strongs = Column(String)
+    transliteration = Column(String)
+
+    verse_words = relationship('VerseWord', order_by='VerseWord.id', back_populates='word')
+    word_morphologies = relationship('WordMorphology', order_by='WordMorphology.id', back_populates='word')
+
+
+class WordMorphology(Base):
+    __tablename__ = 'word_morphology'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    word_id = Column(Integer, ForeignKey('word.id'), nullable=False)
+    index = Column(Integer, nullable=False)
+    part_of_speech = Column(String, nullable=False)
+    word_type = Column(String)
+    stems = Column(String)
     person = Column(String)
     gender = Column(String)
     number = Column(String)
     state = Column(String)
 
+    word = relationship('Word', back_populates='word_morphologies')
+
 
 class VerseWord(Base):
-    __tablename__ = 'verse_words'
+    __tablename__ = 'verse_word'
+    __table_args__ = (UniqueConstraint('book', 'chapter', 'verse', 'verse_position', name='_verse_word_uc'),)
     id = Column(Integer, primary_key=True, autoincrement=True)
     book = Column(String, nullable=False)
     chapter = Column(Integer, nullable=False)
     verse = Column(Integer, nullable=False)
-    strongs = Column(String, nullable=False)
-    word_id = Column(Integer, ForeignKey('words.id'), nullable=False)
-
-    transliteration = Column(String)
-    hebrew = Column(String, nullable=False)
+    verse_position = Column(Integer, nullable=False)
+    word_id = Column(Integer, ForeignKey('word.id'), nullable=False)
     english = Column(String, nullable=False)
 
-    word = relationship('Word', back_populates='verses')
-
-
-Word.verses = relationship('VerseWords', order_by=VerseWords.id, back_populates='word')
+    word = relationship('Word', back_populates='verse_words')
 
 
 DATABASE_URL = 'sqlite:///words.db'
 engine = create_engine(DATABASE_URL)
 Session = sessionmaker(bind=engine)
+
+def get_session():
+    Base.metadata.create_all(engine)
+    return Session()
--- wwi/extract-bible.py
+++ wwi/extract-bible.py
@@ -1,9 +1,134 @@
+'''
+Morphology mapped to the format as per: https://www.blueletterbible.org/resources/morphology/hebrew-codes.cfm
+Words extracted from: https://openbible.com
+Format of morphology: https://biblehub.com/hebrewparse.htm
+'''
+import re
+
 import requests
 from bs4 import BeautifulSoup
 
+from wwi.db import Word, VerseWord, get_session, WordMorphology
+
+
+def extract_root(strongs_number: str):
+    url = f'https://openbible.com/strongs/hebrew/{strongs_number}.htm'
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    root = soup.find('span', class_='hebrew').text.strip()
+    # Define a regular expression pattern to match Hebrew vowel markings
+    vowel_markings_pattern = re.compile(r'[\u0591-\u05C7]')
+    # Remove the vowel markings from the Hebrew word
+    cleaned_word = re.sub(vowel_markings_pattern, '', root)
+    return cleaned_word
+
+
+def _process_morphology(morphology: str):
+    result = []
+    morph_raw_parts = morphology.split('\xa0')
+    morphology_processed = morph_raw_parts[1] if len(morph_raw_parts) > 1 else morphology
+    parts_raw = morphology_processed.split(' | ')
+    parts = []
+    for p in parts_raw:
+        parts_parts = p.split(', ')
+        parts.extend(parts_parts)
+    for ix, p in enumerate(parts):
+        p_parts = p.split('-')
+        part_of_speech = p_parts[0]
+        morphology_part = {'index': ix, 'part_of_speech': part_of_speech}
+        if part_of_speech in 'Conj':
+            if len(p_parts) == 2:
+                morphology_part['type'] = p_parts[1]
+        elif part_of_speech == 'Prep':
+            if len(p_parts) == 2:
+                morphology_part['type'] = p_parts[1]
+        elif part_of_speech == 'Art':
+            assert len(p_parts) == 1
+        elif part_of_speech == 'DirObjM':
+            assert len(p_parts) == 1
+        elif part_of_speech == 'Adv':
+            if len(p_parts) == 2:
+                morphology_part['type'] = p_parts[1]
+        elif part_of_speech == 'Pro' or part_of_speech.startswith(('1', '2', '3')):
+            morphology_part['part_of_speech'] = 'suffix'
+            if len(p_parts) == 1:
+                morphology_part['word_type'] = 'p'
+                assert len(p_parts[0]) == 3
+                morphology_part['person'] = p_parts[0][0]
+                morphology_part['gender'] = p_parts[0][1]
+                morphology_part['number'] = p_parts[0][2]
+            if len(p_parts) == 2:
+                morphology_part['word_type'] = p_parts[1]
+                if part_of_speech.startswith(('1', '2', '3')):
+                    morphology_part['word_type'] = 'p'
+                    morphology_part['person'] = p_parts[1][0]
+                    morphology_part['gender'] = p_parts[1][1]
+                    morphology_part['number'] = p_parts[1][2]
+                else:
+                    morphology_part['word_type'] = p_parts[1]
+                morphology_part['word_type'] = 'p'
+                assert len(p_parts[0]) == 3
+            else:
+                morphology_part['word_type'] = 'p'
+                assert len(p_parts[0]) == 3
+                morphology_part['person'] = p_parts[0][0]
+                morphology_part['gender'] = p_parts[0][1]
+                morphology_part['number'] = p_parts[0][2]
+        elif part_of_speech == 'Pn':
+            morphology_part['part_of_speech'] = 'suffix'
+            morphology_part['word_type'] = 'n'
+        elif part_of_speech == 'V':
+            morphology_part['stems'] = p_parts[1]
+            morphology_part['word_type'] = p_parts[2]
+            if len(p_parts) > 3:
+                if len(p_parts[3]) == 3:
+                    morphology_part['person'] = p_parts[3][0]
+                    morphology_part['gender'] = p_parts[3][1]
+                    morphology_part['number'] = p_parts[3][2]
+                elif len(p_parts[3]) == 2:
+                    morphology_part['gender'] = p_parts[3][0]
+                    morphology_part['number'] = p_parts[3][1]
+        elif part_of_speech in ('N', 'Adj', 'Number'):
+            if p_parts[1] == 'proper':
+                morphology_part['type'] = 'p'
+                morphology_part['gender'] = p_parts[2][0]
+                morphology_part['number'] = p_parts[2][1]
+                if len(p_parts[1]) == 3:
+                    morphology_part['state'] = p_parts[2][2]
+            elif p_parts[1] == 'g':
+                morphology_part['type'] = p_parts[1]
+                morphology_part['number'] = p_parts[2][0]
+                if len(p_parts[1]) == 2:
+                    morphology_part['state'] = p_parts[2][1]
+            elif p_parts[1] == 'o':
+                morphology_part['type'] = p_parts[1]
+                morphology_part['gender'] = p_parts[2][0]
+                morphology_part['number'] = p_parts[2][1]
+                if len(p_parts[1]) == 3:
+                    morphology_part['state'] = p_parts[2][2]
+            else:
+                morphology_part['gender'] = p_parts[1][0]
+                morphology_part['number'] = p_parts[1][1]
+                if len(p_parts[1]) == 3:
+                    morphology_part['state'] = p_parts[1][2]
+        elif part_of_speech in ('I', 'Interrog'):
+            pass
+        elif part_of_speech == 'Number':
+            pass
+        else:
+            print(morphology)
+            raise NotImplementedError(f'Unknown morphology part of speech: {part_of_speech}')
+
+        result.append(morphology_part)
+        word_type = p_parts[0]
+        word_subtype = None
+    person = None
+
+    return result
+
 def extract_verse_data(book: str, chapter: int, verse: int):
     url = f'https://openbible.com/text/{book}/{chapter}-{verse}.htm'
-    print(f'Extracting data from {url}')
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
 
@@ -12,34 +137,97 @@ def extract_verse_data(book: str, chapter: int, verse: int):
     rows = table.find_all('tr')
 
     data = []
-    for row in rows[1:]:  # Skip the header row
+    for ix, row in enumerate(rows[2:]):  # Skip the header row
         cells = row.find_all('td')
         if len(cells) == 4:
             strongs = cells[0].text.strip()
-            hebrew = cells[1].text.strip()
+            root = extract_root(strongs) if strongs else None
+            hebrew_text = cells[1].contents[0].strip()
+            translit_text = cells[1].find('span', class_='translit').find('a').text
             english = cells[2].text.strip()
-            morphology = cells[3].text.strip()
+            morphology = _process_morphology(cells[3].text.strip())
             data.append({
                 'book': book,
                 'chapter': chapter,
                 'verse': verse,
+                'verse_position': ix,
                 'strongs': strongs,
-                'hebrew': hebrew,
+                'root': root,
+                'hebrew': hebrew_text,
+                'transliteration': translit_text,
                 'english': english,
                 'morphology': morphology
             })
     return data
 
+
+def insert_verse_data(session, data):
+    for d in data:
+        verse_word_data = session.query(VerseWord).filter_by(
+            book=d['book'],
+            chapter=d['chapter'],
+            verse=d['verse'],
+            verse_position=d['verse_position']
+        ).first()
+        if verse_word_data:
+            continue
+
+        word_data = session.query(Word).filter_by(root=d['root']).first()
+        if not word_data:
+            word_data = Word(
+                hebrew=d['hebrew'],
+                root=d['root'],
+                language='Hebrew',
+                strongs=d['strongs'],
+                transliteration=d['transliteration']
+            )
+            session.add(word_data)
+            session.commit()
+        else:
+            print(f'Word already exists: {d["hebrew"]} - {d["root"]} - {d["strongs"]}')
+            print(f'In DB: {word_data.hebrew} - {word_data.root} - {word_data.strongs}')
+            assert word_data.hebrew == d['hebrew'] and word_data.root == d['root'] and word_data.strongs == d['strongs']
+
+        for m in d['morphology']:
+            morphology_data = WordMorphology(
+                word_id=word_data.id,
+                index=m['index'],
+                part_of_speech=m['part_of_speech'],
+                word_type=m.get('word_type'),
+                stems=m.get('stems'),
+                person=m.get('person'),
+                gender=m.get('gender'),
+                number=m.get('number'),
+                state=m.get('state')
+            )
+            session.add(morphology_data)
+
+            verse_word_data = VerseWord(
+                book=d['book'],
+                chapter=d['chapter'],
+                verse=d['verse'],
+                verse_position=d['verse_position'],
+                word_id=word_data.id,
+                english=d['english']
+            )
+            session.add(verse_word_data)
+        session.commit()
+
 def extract_book_data(book: str):
     book_data = []
+
+    session = get_session()
 
     for chapter in range(1, 155):
         for verse in range(1, 200):
             chapter_data = extract_verse_data(book, chapter, verse)
             if not chapter_data:
                 return book_data
+            insert_verse_data(session, chapter_data)
             book_data.extend(chapter_data)
-            return book_data
+            print(f'Finished {book}-{chapter}:{verse}')
+        session.commit()
+        return book_data
 
     return book_data
 
